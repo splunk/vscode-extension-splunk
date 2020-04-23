e728774d537bcda7a06c9a3a3b6d085bcdd2de26
Jason Conger <jconger@splunk.com>
2020-04-23 18:50:13 -0500

    Issue #12 and #14

--- out/extension.js
+++ out/extension.js
@@ -153,7 +153,7 @@ function activate(context) {
     });
 
     context.subscriptions.push(vscode.commands.registerCommand('splunk.search.adhoc', async () => {
-        let splunkUrl = vscode.workspace.getConfiguration().get('splunk.commands.restUrl')
+        let splunkUrl = vscode.workspace.getConfiguration().get('splunk.commands.slkunkRestUrl')
         let splunkToken = vscode.workspace.getConfiguration().get('splunk.commands.token')
         let outputMode = vscode.workspace.getConfiguration().get('splunk.search.searchOutputMode')
         
