da66f5f733a5fc81b0b712cc8009c97a7d9cf152
damola <dshomoye@gmail.com>
2021-01-16 15:37:33 -0800

    render changes

--- out/globalConfigPreview.js
+++ out/globalConfigPreview.js
@@ -6,30 +6,36 @@ function init(context) {
     vscode.commands.registerCommand('splunk.previewGlobalConfig', () => previewHanlder(path.join(context.extensionPath, 'resources', 'templates', 'globalConfig.html')))
 }
 
-function previewHanlder(templatePath) {
-    const activeFile = vscode.window.activeTextEditor
-    const configText = activeFile.document.getText()
+function render(text, panel) {
     try {
-        const configObject = JSON.parse(configText)
-        const panel = vscode.window.createWebviewPanel(
-            'splunkWebView',
-            'Global Config Preview',
-            vscode.ViewColumn.Beside,
-            {
-                enableScripts: true,
-            }
-        );
-        const updateView = () => {
-            const template = fs.readFileSync(templatePath, {
-                encoding: "utf-8"
-            });
-            panel.webview.html = template
-        }
-        updateView();
+        const configObject = JSON.parse(text)
         panel.webview.postMessage({ action: 'config-data', data: configObject })
     } catch (e) {
         console.error("Error Rendering preview. ", e.message)
     }
+}
+
+function previewHanlder(templatePath) {
+    const panel = vscode.window.createWebviewPanel(
+        'splunkWebView',
+        'Global Config Preview',
+        vscode.ViewColumn.Beside,
+        {
+            enableScripts: true,
+        }
+    );
+    const template = fs.readFileSync(templatePath, {
+        encoding: "utf-8"
+    });
+    panel.webview.html = template
+    const configText = vscode.window.activeTextEditor.document.getText()
+    render(configText, panel);
+    vscode.workspace.onDidChangeTextDocument((e) => {
+        const doc = e.document
+        if (doc.fileName.endsWith("globalConfig.json")) {
+            render(doc.getText(), panel)
+        }
+    })
 }
 
 exports.init = init;
