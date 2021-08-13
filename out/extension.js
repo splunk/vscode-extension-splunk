26da4f598f6f474a038250f7de89214c3a6625cd
Jason Conger <jason@jasonconger.com>
2021-08-12 21:02:43 -0500

    refactor: update REST dependency

--- out/extension.js
+++ out/extension.js
@@ -4,8 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 const path = require("path");
 const fs = require("fs");
-const request = require("request");
-const splunkSavedSearchProvider = require("./savedSearchProvider.js");
+const splunkSearchProvider = require("./searchProvider.js");
 const splunkEmbeddedReportProvider = require("./embeddedReportProvider");
 const splunkFoldingRangeProvider = require("./foldingRangeProvider.js");
 const splunkModViz = require('./modViz.js');
@@ -13,10 +12,10 @@ const splunkCustomCommand = require('./customCommand.js');
 const globalConfigPreview = require('./globalConfigPreview')
 const splunkCustomRESTHandler = require('./customRESTHandler.js')
 const splunkSpec = require("./spec.js");
-const { get } = require("request");
+const { AsyncLocalStorage } = require("async_hooks");
 const PLACEHOLDER_REGEX = /\<([^\>]+)\>/g
 const DROPDOWN_PLACEHOLDER_REGEX = /(\[|{)\w+(\|\w+)+(]|})/g
-const STANZA_REGEX = /^\[(?<stanza>[^\]].*?)\]/
+const STANZA_REGEX = /^\[(?<stanza>|[^\]].*?)\]/
 const SETTING_REGEX = /^(?<setting>[\w\-_\<\>\.]+)\s*=\s*(?<value>[^\r\n]+)/
 let specConfigs = {};
 let timeout = undefined;
@@ -92,8 +91,8 @@ function activate(context) {
         
     });
 
-    // Set up Splunk saved search viewer
-    const savedSearchProvider = new splunkSavedSearchProvider.SavedSearchProvider();
+    // Set up Splunk search viewer
+    const savedSearchProvider = new splunkSearchProvider.SavedSearchProvider();
     vscode.window.registerTreeDataProvider('savedSearches', savedSearchProvider);
     vscode.commands.registerCommand('splunk.savedSearches.refresh', () => savedSearchProvider.refresh());
     vscode.commands.registerCommand('splunk.savedSearch.run', async search => {
@@ -106,44 +105,15 @@ function activate(context) {
     });
 
     // Set up Splunk ad-hoc search command
+    const searchProvider = new splunkSearchProvider.SearchProvider();
     context.subscriptions.push(vscode.commands.registerCommand('splunk.search.adhoc', async () => {
-        let splunkUrl = vscode.workspace.getConfiguration().get('splunk.commands.splunkRestUrl')
-        let splunkToken = vscode.workspace.getConfiguration().get('splunk.commands.token')
-        let outputMode = vscode.workspace.getConfiguration().get('splunk.search.searchOutputMode')
-        
-        if (!splunkUrl) {
-            vscode.window.showErrorMessage("The URL specified for the Splunk REST API is incorrect. Please check your settings.");
-            return
-        }
-        if(!splunkToken) {
-            vscode.window.showErrorMessage("A Splunk autorization token is required. Please check your settings.");
-            return
-        }
         let search = await vscode.window.showInputBox({
             prompt:'Search SPL'
         })
         if (search) {
-            request(
-                {
-                    method: "POST",
-                    uri: `${splunkUrl}/services/search/jobs/export?output_mode=${outputMode}`,
-                    strictSSL: false,
-                    headers : {
-                        "Authorization": `Bearer ${splunkToken}`
-                    },
-                    body: "search=" + encodeURIComponent(`search ${search}`)
-                    
-                },
-                function (error, response, body) {
-                    if(error) {
-                        vscode.window.showErrorMessage(error.message)
-                    } else {
-                        splunkOutputChannel.appendLine(body)
-                    }
-                }
-            )
-
-            splunkOutputChannel.show()
+            let searchResult = await searchProvider.runSearch(search);
+            splunkOutputChannel.appendLine(searchResult);
+            splunkOutputChannel.show();
         }
     }));
 
@@ -225,9 +195,9 @@ function activate(context) {
         { language: 'splunk', pattern: '**/*.{conf,conf.spec}' }
     ], new splunkFoldingRangeProvider.confFoldingRangeProvider()));
 
-    // If vscode was opened with an active .conf file, handle it.
+    // If vscode was opened with an active .conf file or metadata file, handle it.
     if(vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId === 'splunk') {
-        handleSplunkConfFile(context);
+        handleSplunkSpecFile(context);
     }
 
     // Set up listener for text document changes
@@ -241,24 +211,23 @@ function activate(context) {
     // Set up listener for active editor changing
     context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
         if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId === 'splunk') {
-            handleSplunkConfFile(context);
+            handleSplunkSpecFile(context);
         }
     }));
 }
 exports.activate = activate;
 
-function handleSplunkConfFile(context) {
+function handleSplunkSpecFile(context) {
 
     if(diagnosticCollection === undefined) {
         diagnosticCollection = vscode.languages.createDiagnosticCollection('splunk');
     }
 
     let currentDocument = path.basename(vscode.window.activeTextEditor.document.uri.fsPath);
-    let specFileName = currentDocument + ".spec";
     let specFilePath = getSpecFilePath(context.extensionPath, currentDocument);
 
-    if(specConfigs.hasOwnProperty(specFileName)) {
-        specConfig = specConfigs[specFileName];
+    if(specConfigs.hasOwnProperty(currentDocument)) {
+        specConfig = specConfigs[currentDocument];
     } else {
         specConfig = splunkSpec.getSpecConfig(specFilePath);
 
@@ -274,7 +243,7 @@ function handleSplunkConfFile(context) {
     updateDiagnostics(specConfig, vscode.window.activeTextEditor.document, diagnosticCollection);
 
     // Cache specConfig
-    specConfigs[specFileName] = specConfig
+    specConfigs[currentDocument] = specConfig
 }
 
 function getSpecFilePath(basePath, filename) {
@@ -282,23 +251,25 @@ function getSpecFilePath(basePath, filename) {
     // Get the custom configuration options
     let settingsSpecFilePath = vscode.workspace.getConfiguration('splunk').get('spec.FilePath');
     let settingsSpecFileVersion  = vscode.workspace.getConfiguration('splunk').get('spec.FileVersion');
-
     let specFileName = filename + ".spec";
-    let specFilePath;
+
+    // Special case spec files
+    let specialSpecFiles = ["eventgen.conf.spec", "default.meta.spec", "local.meta.spec"]
+    if(specialSpecFiles.indexOf(specFileName)) {
+        if (specFileName == "local.meta.spec") { specFileName = "default.meta.spec" }
+        return checkSpecFilePath(path.join(basePath, "spec_files", specFileName))
+    }
 
     // Create a path to the spec file for the current document
     if(!settingsSpecFilePath) {
         // No path was configured in settings, so create a path to the built-in spec files
-        if(specFileName == "eventgen.conf.spec") {
-            specFilePath = path.join(basePath, "spec_files", specFileName)
-        } else {
-            specFilePath = path.join(basePath, "spec_files", settingsSpecFileVersion, specFileName)
-        }
+        return checkSpecFilePath(path.join(basePath, "spec_files", settingsSpecFileVersion, specFileName))
     } else {
-        specFilePath = path.join(specFilePath, specFileName)
+        return checkSpecFilePath(path.join(specFilePath, specFileName))
     }
+}
 
-    // Check if the file exists
+function checkSpecFilePath(specFilePath) {
     if(!fs.existsSync(specFilePath)) {
         vscode.window.showErrorMessage(`Spec file path not found: ${specFilePath}`)
         return null
