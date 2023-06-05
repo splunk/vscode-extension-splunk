b77ec9d691da98d6a78c7d7bcc2c9b0678d70a83
Tim Pavlik <tpavlik@splunk.com>
2023-06-05 16:07:09 -0700

    Refactor for DRYness.

--- out/notebooks/spl2/controller.ts
+++ out/notebooks/spl2/controller.ts
@@ -1,76 +1,37 @@
 import * as vscode from 'vscode';
 
 import {
-    cancelSearchJob,
     dispatchSpl2Module,
     getClient,
-    getSearchJob,
-    getSearchJobResults,
-    wait,
 } from '../splunk';
+import { SplunkController } from '../controller';
 import { splunkMessagesToOutputItems } from '../utils';
 
 // TODO: refactor to inherit/compose with SplunkController for DRYness
-export class Spl2Controller {
-    readonly controllerId = 'spl2-notebook-controller';
-    readonly notebookType = 'spl2-notebook';
-    readonly label = 'SPL2 Note';
-    readonly supportedLanguages = ['splunk_spl2'];
-
-    private readonly _controller: vscode.NotebookController;
-    private _executionOrder = 0;
-    private _interrupted = false;
-
+export class Spl2Controller extends SplunkController {
     constructor() {
-        this._controller = vscode.notebooks.createNotebookController(
-            this.controllerId,
-            this.notebookType,
-            this.label,
-        );
-
-        this._controller.supportedLanguages = this.supportedLanguages;
-        this._controller.supportsExecutionOrder = true;
+        super('spl2-notebook-controller', 'spl2-notebook', 'SPL2 Note', ['splunk_spl2']);
         this._controller.executeHandler = this._execute.bind(this);
     }
 
-    dispose(): void {
-        this._controller.dispose();
-    }
-
-    async runCell(cell: vscode.NotebookCell) {
-        const notebookDocument = await vscode.workspace.openNotebookDocument(cell.document.uri)
-        this._execute([cell], notebookDocument, this._controller);
-    }
-
-    private _execute(
+    protected _execute(
         cells: vscode.NotebookCell[],
         _notebook: vscode.NotebookDocument,
         _controller: vscode.NotebookController
     ): void {
         for (let cell of cells) {
             if (cell.document.languageId == 'splunk_spl2') {
-                this._doExecution(cell);
+                this._doSpl2Execution(cell);
             }
         }
     }
 
-    async interruptHandler(notebook: vscode.NotebookDocument): Promise<void> {
-        console.log('interrupt handler called');
-    }
-
-    private async _doExecution(cell: vscode.NotebookCell): Promise<void> {
-        this._interrupted = false;
-        console.log(cell);
-        const execution = this._controller.createNotebookCellExecution(cell);
-        execution.executionOrder = ++this._executionOrder;
-        execution.start(Date.now());
+    private async _doSpl2Execution(cell: vscode.NotebookCell): Promise<void> {
+        const execution = super._startExecution(cell);
 
         const spl2Module = cell.document.getText().trim();
         const service = getClient();
     
-        let activeThemeKind = vscode.window.activeColorTheme.kind;
-        let backgroundColor = new vscode.ThemeColor('notebook.editorBackground');
-
         let job;
         try {
             job = await dispatchSpl2Module(service, spl2Module);
@@ -83,64 +44,6 @@ export class Spl2Controller {
             return;
         }
 
-        execution.replaceOutput([new vscode.NotebookCellOutput([], { job: job.properties() })]);
-
-        execution.token.onCancellationRequested(async () => {
-            this._interrupted = true;
-
-            let res: any = await cancelSearchJob(job);
-
-            const messageItems = splunkMessagesToOutputItems(res.data.messages);
-            execution.replaceOutput([new vscode.NotebookCellOutput(messageItems, { job: job.properties() })]);
-
-            execution.end(undefined, Date.now());
-        });
-
-        let jobComplete = false;
-
-        while (!jobComplete && !this._interrupted) {
-            job = await getSearchJob(job);
-
-            if (job.properties().isDone == true) {
-                jobComplete = true;
-                continue;
-            } 
-            wait(1000);
-        }        
-
-        if (job.properties().isFailed == true) {
-            const messages = job.properties().messages;
-            const messageItems = splunkMessagesToOutputItems(messages);
-
-            execution.replaceOutput([new vscode.NotebookCellOutput(messageItems, { job: job.properties() })]);
-            execution.end(false, Date.now());
-            return
-        }
-
-        if (!this._interrupted) {
-            let results: any = await getSearchJobResults(job);
-
-            execution.replaceOutput([
-                new vscode.NotebookCellOutput(
-                    [
-                        vscode.NotebookCellOutputItem.json(
-                            {
-                                results: results.data,
-                                _meta: {
-                                    backgroundColor: backgroundColor,
-                                    colorMode: activeThemeKind,
-                                    cellMeta: cell.metadata,
-                                },
-                            },
-                            'application/splunk/events'
-                        ),
-                        vscode.NotebookCellOutputItem.json(job.properties()),
-                    ],
-                    { job: job.properties() }
-                ),
-            ]);
-
-            execution.end(true, Date.now());
-        }
+        await super._finishExecution(job, cell, execution);
     }
 }
